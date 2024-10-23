6c793df01ed2c521e1b9499e3923db99217cd365
Chris Siebenmann <cks.git02@cs.toronto.edu>
2024-10-23 19:26:59 -0400

    Add a -n option to run multiple DNS lookups in parallel
    This is implemented with brute force restructuring, where our lookup
    routines now take an io.Writer to write to. In the non-parallel case
    this is io.Stdout; in the parallel case these are bytes.Buffers that
    are printed later.

--- main.go
+++ main.go
@@ -18,11 +18,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/pborman/getopt/v2"
@@ -31,6 +34,7 @@ import (
 // Sometimes globals are the simplest answer.
 var allnames, quiet, nounknown, verbose, novalidate, help, readstdin bool
 var tmout time.Duration
+var npar int
 
 // validateIpName validates one name that is supposed to mape to a given IP
 // (provided in string form). It returns either true or false, and if it
@@ -69,10 +73,11 @@ func validateIpName(ctx context.Context, ips, name string) (ok bool, reason stri
 }
 
 // checkIp checks one IP address and prints the results.
+// The results are printed to the io.Writer we're given.
 //
 // TODO: should we support a timeout for the entire process, in
 // addition to a timeout on each DNS request? Tentatively no.
-func checkIp(ips string) {
+func checkIp(ips string, to io.Writer) {
 	// If we don't force PreferGo, we can only ever get one
 	// result for the IP to name lookup, apparently.
 	res := &net.Resolver{
@@ -83,7 +88,7 @@ func checkIp(ips string) {
 	if ip == nil {
 		// We always report this result in full even if you
 		// asked for quiet results because lol no.
-		fmt.Printf("%s NOT-IP-ADDRESS\n", ips)
+		fmt.Fprintf(to, "%s NOT-IP-ADDRESS\n", ips)
 		return
 	}
 	// We deliberately canonicalize the IP address here.
@@ -105,7 +110,7 @@ func checkIp(ips string) {
 	r, err := res.LookupAddr(ctx, ips)
 	if len(r) == 0 {
 		if !quiet && !nounknown {
-			fmt.Printf("%s UNKNOWN\n", ips)
+			fmt.Fprintf(to, "%s UNKNOWN\n", ips)
 		}
 		return
 	}
@@ -114,7 +119,7 @@ func checkIp(ips string) {
 	// in current Go (1.21). We report this even if validation is
 	// off, for reasons.
 	if err != nil && !quiet {
-		fmt.Printf("%s BAD-PTR-DATA %s\n", ips, err)
+		fmt.Fprintf(to, "%s BAD-PTR-DATA %s\n", ips, err)
 	}
 
 	// If we are not reporting all names and we are validating, we
@@ -128,13 +133,13 @@ func checkIp(ips string) {
 		hn2 = strings.TrimSuffix(hn, ".")
 		switch {
 		case ok && verbose:
-			fmt.Printf("%s %s\n", ips, hn2)
+			fmt.Fprintf(to, "%s %s\n", ips, hn2)
 		case ok:
-			fmt.Printf("%s\n", hn2)
+			fmt.Fprintf(to, "%s\n", hn2)
 		case !ok && !allnames:
 			// We do nothing ... yet.
 		case !ok && !quiet:
-			fmt.Printf("%s %s %s\n", ips, reason, hn2)
+			fmt.Fprintf(to, "%s %s %s\n", ips, reason, hn2)
 		default:
 			// We are silent on failures with quiet.
 		}
@@ -143,8 +148,115 @@ func checkIp(ips string) {
 		}
 	}
 	if !allnames && !quiet && !ok {
-		fmt.Printf("%s %s %s\n", ips, reason, hn2)
+		fmt.Fprintf(to, "%s %s %s\n", ips, reason, hn2)
 	}
+}
+
+// A DNS lookup request is a struct with the IP to look up and a channel
+// to send the result. A DNS reply is a channel of *bytes.Buffer, which
+// will receive exactly one message.
+//
+// An industrial version would probably pass a context around too.
+type dnsReply chan *bytes.Buffer
+type dnsSend struct {
+	ips   string
+	reply dnsReply
+}
+
+// the promises slice is only used in the main thread.
+type dnsCheckPool struct {
+	requests chan dnsSend   // common channel for requests
+	wg       sync.WaitGroup // signals all resolvers have exited
+	promises []dnsReply     // accumulated replies (channels)
+}
+
+// checkIp is called from the main thread to initiate the check of one
+// IP address. It submits the request to the pool and adds the reply
+// channel to the promises slice.
+func (d *dnsCheckPool) checkIp(ips string) {
+	if d == nil {
+		checkIp(ips, os.Stdout)
+		return
+	}
+	// the reply channel must have a buffer so that resolvers never
+	// block while writing answers to it.
+	ds := dnsSend{
+		ips:   ips,
+		reply: make(dnsReply, 1),
+	}
+	d.requests <- ds
+	d.promises = append(d.promises, ds.reply)
+}
+
+// resolver is run in N goroutines to read requests, perform DNS
+// lookups, and send the reply to the reply channel. The reply channel
+// must have a one-item buffer so that the resolver doesn't block in
+// replying.
+func (d *dnsCheckPool) resolver() {
+	for ds := range d.requests {
+		var b bytes.Buffer
+		checkIp(ds.ips, &b)
+		ds.reply <- &b
+	}
+}
+
+// drain reads and prints the first N results, assuming that there are
+// at least N available. It's intended to let you avoid having too many
+// unprocessed results pile up.
+func (d *dnsCheckPool) drain(num int) {
+	if d == nil || len(d.promises) < num {
+		return
+	}
+	for i := range d.promises[:num] {
+		b := <-d.promises[i]
+		_, _ = b.WriteTo(os.Stdout)
+		close(d.promises[i])
+	}
+	d.promises = d.promises[num:]
+}
+
+// Finish a DNS check pool, reading and printing all results and
+// closing down the resolver goroutines.
+func (d *dnsCheckPool) finish() {
+	if d == nil {
+		return
+	}
+	// signal resolvers to exit but don't wait for them yet
+	close(d.requests)
+	// receive and print results, many of which will already be
+	// ready (we hope).
+	for i := range d.promises {
+		b := <-d.promises[i]
+		_, _ = b.WriteTo(os.Stdout)
+		close(d.promises[i])
+	}
+	// help the garbage collector out.
+	clear(d.promises)
+	// wait for resolver goroutines to all signal completion.
+	d.wg.Wait()
+}
+
+// newDnsCheckPool creates a new DNS IP resolution pool that is expected
+// to handle (at least) 'size' entries.
+func newDnsCheckPool(size int) *dnsCheckPool {
+	if npar <= 1 {
+		// no parallelism? arrange to call directly.
+		return nil
+	}
+	var d = &dnsCheckPool{}
+	// Requests are all submitted before any replies are printed,
+	// so we'd like to not have that submission block. So size the
+	// requests channel for our expected maximum capacity.
+	d.requests = make(chan dnsSend, size)
+	// Don't start extra goroutines if we have too few requests.
+	for range min(npar, size) {
+		d.wg.Add(1)
+		go func() {
+			d.resolver()
+			d.wg.Done()
+		}()
+	}
+	return d
 }
 
 func main() {
@@ -156,6 +268,7 @@ func main() {
 	getopt.FlagLong(&nounknown, "no-unknown", 'U', "Don't report IPs that have no reverse DNS names.")
 	getopt.FlagLong(&tmout, "timeout", 't', "Timeout for each DNS lookup, if any; use eg '1s'.", "DURATION")
 	getopt.FlagLong(&readstdin, "stdin", 'S', "Read IPs to resolve from standard input (one per line) instead of arguments.")
+	getopt.FlagLong(&npar, "parallel", 'n', "Perform this many DNS lookups in parallel.", "NUM")
 	getopt.SetParameters("IP [IP ...]")
 
 	getopt.Parse()
@@ -176,15 +289,22 @@ func main() {
 		return
 	}
 
+	pl := newDnsCheckPool(max(len(args), npar))
 	if len(args) > 0 {
 		for _, ips := range args {
-			checkIp(ips)
+			//checkIp(ips, os.Stdout)
+			pl.checkIp(ips)
 		}
+		pl.finish()
 	} else {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
-			checkIp(scanner.Text())
+			//checkIp(scanner.Text(), os.Stdout)
+			pl.checkIp(scanner.Text())
+			// Don't allow too many to build up.
+			pl.drain(npar * 3)
 		}
+		pl.finish()
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintf(os.Stderr, "%s: reading standard input: %s\n", os.Args[0], err)
 		}
